526b016c8f680637b639aea47cbf369cc9526976
Ryan Wolfe <rwolfe22@gmail.com>
2020-04-18 21:50:20 -0700

    Updates

---
+++ precache-manifest.2b5c2c3d88ffa0c3b4924299fcc4c7d3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5930c8542810da92fbd99d38b71802",
+    "url": "/adidas-cart/index.html"
+  },
+  {
+    "revision": "5a6387a854f8f62fd540",
+    "url": "/adidas-cart/static/css/2.d5d0a1e1.chunk.css"
+  },
+  {
+    "revision": "aaa36e00200e5b930087",
+    "url": "/adidas-cart/static/css/main.8a225959.chunk.css"
+  },
+  {
+    "revision": "5a6387a854f8f62fd540",
+    "url": "/adidas-cart/static/js/2.319a306a.chunk.js"
+  },
+  {
+    "revision": "7919130a4cf3aff0f4075975f626da52",
+    "url": "/adidas-cart/static/js/2.319a306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa36e00200e5b930087",
+    "url": "/adidas-cart/static/js/main.07a1d3ec.chunk.js"
+  },
+  {
+    "revision": "700d66cdb52b07229e1d",
+    "url": "/adidas-cart/static/js/runtime-main.3f14aa4e.js"
+  },
+  {
+    "revision": "167f535e2327fef1d7336982d8458021",
+    "url": "/adidas-cart/static/media/arrow.167f535e.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/adidas-cart/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/adidas-cart/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/adidas-cart/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/adidas-cart/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/adidas-cart/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "f987d4e1000147da9121a0391f66771b",
+    "url": "/adidas-cart/static/media/cross.f987d4e1.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/adidas-cart/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/adidas-cart/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/adidas-cart/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/adidas-cart/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/adidas-cart/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/adidas-cart/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "798ff6834556e20e0d0307fc4ff24c36",
+    "url": "/adidas-cart/static/media/menu.798ff683.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/adidas-cart/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/adidas-cart/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/adidas-cart/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/adidas-cart/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/adidas-cart/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
